d1e940e8e9147b394d24c08c054a63ce192ad81b
RobiAdawiya <robiadawiyah520@gmail.com>
2026-01-27 08:23:59 +0700

    modify api.py backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 const BASE_URL = "http://127.0.0.1:8000"; 
 
 export const validateManpower = async (nik, nama) => {
-  // Mengirim ke: http://127.0.0.1:8000/validate/manpower?nik=337&nama=syafina
   const response = await fetch(`${BASE_URL}/validate/manpower?nik=${nik}&nama=${nama}`);
   return await response.json();
 };
